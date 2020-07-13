e859c73ee6f084c9705c36d6f32f411f910bbbae
marcelobruckner <marcelobruckner@gmail.com>
2020-07-13 16:46:22 -0300

    atualizando arquivos de codigo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,46 @@
-var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
-console.log(frutas);
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<i>Muito obrigado.</i>";
+}
+
+function redirecionar(){
+    window.open("https://web.digitalinnovation.one/");
+}
+/*
+var data = new Date();
+console.log(data.getDate());
+console.log(data.getMonth()+1);
+console.log(data.getMinutes());
+console.log(data.getHours());
+*/
+/*
+var lista  = [nome: "rosa" , "cravo" , "tulipa", "margarida"];
+console.log(lista[2]);
+*/
+
+/*
+var count;
+for(count=0; count<=5;count++){
+    console.log(count);
+}
+*/
+/*var count = 0;
+while(count <=5){
+    console.log(count);
+    count++;
+}*/
 
 
+/*
+var idade = prompt("Qual a sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+}
+*/
 
-
-
+//var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+//console.log(frutas);
 //var lista = ["maçã", "pêra", "laranja"];
 //alert(lista);
 //lista.push("uva");
