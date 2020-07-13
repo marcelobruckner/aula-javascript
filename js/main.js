9e80252d6dc10579b7e1989b2a8c14e3b23644f5
marcelobruckner <marcelobruckner@gmail.com>
2020-07-13 11:45:31 -0300

    commitando aquivos iniciais

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+
+
+
+
+
+//var lista = ["maçã", "pêra", "laranja"];
+//alert(lista);
+//lista.push("uva");
+//alert(lista);
+//lista.pop();
+//alert(lista);
+//alert(lista.join(" - "));
